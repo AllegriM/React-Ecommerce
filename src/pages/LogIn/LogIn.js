6c78701a20154f5c0218fd963daeaa94fe72f145
AllegriM <elmonky99@gmail.com>
2022-07-06 18:28:28 -0300

    eliminating company logos due to deployment issues

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,8 +1,10 @@
-import { Box, Container, Img } from "@chakra-ui/react"
+import { Box, Container } from "@chakra-ui/react"
+// import { Img } from "@chakra-ui/react"
+
 import { useState } from "react"
 import { LogInForm } from "../../components/LogInForm"
 import { RegisterForm } from "../../components/RegisterForm"
-import logo from '../../imgs/logo__large_plus.png'
+// import logo from '../../imgs/logo__large_plus.png'
 import './login.css'
 
 export const LogIn = () => {
@@ -12,7 +14,7 @@ export const LogIn = () => {
     return (
         <Container backgroundColor='#EBEBEB' h='100%' minH='100vh' w='100%' maxW='100%' p='0' position='relative' display='flex' justifyContent='center'>
             <Box backgroundColor='#FFF059' h='250px' w='100%'>
-                <Img src={logo} ml='1em' mt='1em' />
+                {/* <Img src={logo} ml='1em' mt='1em' /> */}
             </Box>
             <Box  top='10%' position='absolute' margin='0 auto' boxShadow='0 1px 2px 0 rgb(0 0 0 / 30%)' borderRadius='6px' flex backgroundColor='#FFFFFF' minH='34rem' w='300px' maxW='424px' padding='32px 64px'>
                 {
