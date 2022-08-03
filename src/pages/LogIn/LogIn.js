735a4d8bef81aec44ac4b94e79817ad2450959b2
AllegriM <elmonky99@gmail.com>
2022-08-03 19:16:31 -0300

    change login logo

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,10 +1,9 @@
-import { Box, Container, Img } from "@chakra-ui/react"
+import { Box, Container, Text } from "@chakra-ui/react"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { LogInForm } from "../../components/LogInForm"
 import { RegisterForm } from "../../components/RegisterForm"
 import { useAuth } from "../../Context/authContext"
-import logo from '../../imgs/logo__large_plus.png'
 import './login.css'
 
 export const LogIn = () => {
@@ -20,7 +19,7 @@ export const LogIn = () => {
                     :
                     <Container backgroundColor='#EBEBEB' h='100%' minH='100vh' w='100%' maxW='100%' p='0' position='relative' display='flex' justifyContent='center' >
                         <Box backgroundColor='#FFF059' h='250px' w='100%'>
-                            <Img src={logo} ml='1em' mt='1em' />
+                            <Text fontWeight='900' m='0' color='#28317E'>CadoMerLibre</Text>
                         </Box>
                         <Box top='10%' position='absolute' margin='0 auto' boxShadow='0 1px 2px 0 rgb(0 0 0 / 30%)' borderRadius='6px' flex backgroundColor='#FFFFFF' minH='34rem' w='300px' maxW='424px' padding='32px 64px'>
                             {
