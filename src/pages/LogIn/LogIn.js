035bb0ed5416092032dd5dc6dba45b8f28a5667c
AllegriM <elmonky99@gmail.com>
2022-08-03 19:20:19 -0300

    login changed logo

--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -18,8 +18,8 @@ export const LogIn = () => {
                 user !== null ? navigate("/home")
                     :
                     <Container backgroundColor='#EBEBEB' h='100%' minH='100vh' w='100%' maxW='100%' p='0' position='relative' display='flex' justifyContent='center' >
-                        <Box backgroundColor='#FFF059' h='250px' w='100%'>
-                            <Text fontWeight='900' m='0' color='#28317E'>CadoMerLibre</Text>
+                        <Box backgroundColor='#FFF059' h='250px' w='100%' >
+                            <Text p={5} fontWeight='900' m='0' color='#28317E' fontSize='1.5rem'>CadoMerLibre</Text>
                         </Box>
                         <Box top='10%' position='absolute' margin='0 auto' boxShadow='0 1px 2px 0 rgb(0 0 0 / 30%)' borderRadius='6px' flex backgroundColor='#FFFFFF' minH='34rem' w='300px' maxW='424px' padding='32px 64px'>
                             {
